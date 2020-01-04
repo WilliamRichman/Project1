dfc3b2f4b6441292d4ebf52d313fcd0eb12ff3de
William Richman <Goat1120@gmail.com>
2020-01-04 11:59:58 -0700

    working weather API

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     //Not the real url ist a place holder
-    const weather = "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=15976d84b292c4206f0104225b002459"
+    const weather = "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&appid=15976d84b292c4206f0104225b002459"
     const zip = $("<button>");
 
     $('.buttons').click(function(zip){
