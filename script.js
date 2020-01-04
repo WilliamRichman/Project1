4522d149c363517422f66cb26d06779232fe015a
Zac Eskridge <zace118@gmail.com>
2020-01-04 14:10:13 -0700

    Added the onclick function and some notes

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 const cityName = $('#searchBar').val();
 
-$.ajax({
-    url:'https://developers.zomato.com/api/v2.1/locations?query=' + cityName,
-    method: "GET",
-    headers: {
-        'user-key': 'b5075be7d6cf56502c175fb9e26c2396'
-    }
-}).then(function(response){
-    console.log(response);
+$('#searchBar').on('click', function(){
 
-    const cityID = response.location_suggestions[0].city_id;
-    const lat = response.location_suggestions[0].latitude;
-    const lon = response.location_suggestions[0].longitude;
-    
-
+//The initial AJAX call that identifies the user's searched city, grabs the latitude, longitude, and cityID.    
     $.ajax({
-        url: 'https://developers.zomato.com/api/v2.1/search?entity_id=' + cityID + '&entity_type=city&count50',
-        method: 'GET',
+        url:'https://developers.zomato.com/api/v2.1/locations?query=' + cityName,
+        method: "GET",
         headers: {
             'user-key': 'b5075be7d6cf56502c175fb9e26c2396'
         }
-    }).then(function(res){
-        console.log(res);
-
-        const arrayLength = res.results_shown;
-        const randRest = res.restaurants[Math.floor(Math.random()*(arrayLength))];
-        const restName = randRest.restaurant.name;
-        const restAddress = randRest.restaurant.location.address;
-        const restZip = restAddress.slice(-5);
-        const restHours = randRest.restaurant.timings;
-        const restAVGfor2 = randRest.restaurant.average_cost_for_two;
-        const restMenuLink = randRest.restaurant.menu_url;
+    }).then(function(response){
+        console.log(response);
+    
+        const cityID = response.location_suggestions[0].city_id;
+        const lat = response.location_suggestions[0].latitude;
+        const lon = response.location_suggestions[0].longitude;
         
+    //This AJAX call searches through Zomato given the cityID from the previous call and will return one random restaurant.
+        $.ajax({
+            url: 'https://developers.zomato.com/api/v2.1/search?entity_id=' + cityID + '&entity_type=city&count50',
+            method: 'GET',
+            headers: {
+                'user-key': 'b5075be7d6cf56502c175fb9e26c2396'
+            }
+        }).then(function(res){
+            console.log(res);
+    
+            const arrayLength = res.results_shown;
+            const randRest = res.restaurants[Math.floor(Math.random()*(arrayLength))];
+            const restName = randRest.restaurant.name;
+            const restAddress = randRest.restaurant.location.address;
+            const restZip = restAddress.slice(-5);
+            const restHours = randRest.restaurant.timings;
+            const restAVGfor2 = randRest.restaurant.average_cost_for_two;
+            const restMenuLink = randRest.restaurant.menu_url;
+            
+        })
+    
     })
 
-})
+});
+
+
