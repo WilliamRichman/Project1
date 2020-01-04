ed29e26efbc54e829ae3f124bdb786a6d18f1266
William Richman <Goat1120@gmail.com>
2020-01-04 13:06:11 -0700

    JS working fully and appending

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-    //Not the real url ist a place holder
+    
     const weather = "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459"
-    const zip = $("<button>");
+    const zip = $("<p>");
 
     $('.buttons').click(function(zip){
 
@@ -17,6 +17,11 @@
 
    const rain = response.weather[0].main;
    console.log(rain)
+
+    //add the temp and rain to the html page
+    $('.buttons').append(temp)
+    //use the append function to test it to add to .buttons
+    $('.buttons').append(rain)
 
 
 
