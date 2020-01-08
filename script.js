cec255dd16fc4332a329729bb6f2978cf02a19f9
William Richman <Goat1120@gmail.com>
2020-01-07 19:37:18 -0700

    Clean console

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const cityName = 'flagstaff, az'
 
 
 
-//Front-end css stuff
+/* //Front-end css stuff
 document.addEventListener('DOMContentLoaded', function(){
     let stars = document.querySelectorAll('.star');
     stars.forEach(function(star){
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let rating = parseInt(document.querySelector('.stars').getAttribute('data-rating'));
     let target = stars[rating - 1];
     target.dispatchEvent(new MouseEvent('click'));
-});
+}); */
 /* function setRating(ev){
     let span = ev.currentTarget;
     let stars = document.querySelectorAll('.star');
