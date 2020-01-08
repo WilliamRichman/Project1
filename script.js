04e3481bd069619b37767323471fbdc6fc525a3e
Matt Cluff <matthewrcluff@gmail.com>
2020-01-07 20:14:09 -0700

    merged everything

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-    
-    const weather = "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459"
-    const zip = $("<p>");
+const cityName = $('#searchBar').val();
+// const cityName = 'flagstaff, az';
 
-    $('.buttons').click(function(zip){
-
-    });
-
-   $.ajax({
-       url: weather,
-       method: "GET"
-   }).then(function(response){
-    console.log(response);
-    
-   const temp = response.main.temp;
-   console.log(temp);
-
-   const rain = response.weather[0].main;
-   console.log(rain)
-
-    //add the temp and rain to the html page
-    $('.buttons').append(temp)
-    //use the append function to test it to add to .buttons
-    $('.buttons').append(rain)
-   });
-// const cityName = $('#searchBar').val();
-const cityName = 'flagstaff, az'
 //The onclick function that starts the page's seach from the search box.
 $('#generate').on('click', function(){
     const cityName = $('#searchBar').val();
@@ -65,9 +41,29 @@ $('#generate').on('click', function(){
             const restPrice = randRest.restaurant.price_range;
             const restAVGfor2 = randRest.restaurant.average_cost_for_two;
             const restMenuLink = randRest.restaurant.menu_url;
-            
-        })
-    
-    })
+            console.log(restName);
+            console.log(restAddress);
+            console.log(restZip);
+            console.log(restHours);
+            console.log(restPrice);
+            console.log(restAVGfor2);
+            console.log(restMenuLink);
 
+
+            $.ajax({
+                url: 'https://api.openweathermap.org/data/2.5/weather?zip=' + restZip + ',us&units=imperial&appid=15976d84b292c4206f0104225b002459',
+                method: "GET"
+            }).then(function(response){
+                console.log(response);
+                const currentTemp = response.main.temp;
+                console.log(currentTemp);
+         
+                const currentWeather = response.weather[0].main;
+                console.log(currentWeather);
+            });
+
+        });
+    
+    });
+  
 });
