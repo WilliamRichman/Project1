5a6131f5aeec55426adf59a11cf138e64ed9ad57
Zac Eskridge <zace118@gmail.com>
2020-01-07 19:01:18 -0700

    Checkpoint

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// const cityName = $('#searchBar').val();
-const cityName = 'mesa, az'
-
 //The onclick function that starts the page's seach from the search box.
 $('#generate').on('click', function(){
+    const cityName = $('#searchBar').val();
+    // const cityName = 'mesa, az'
+    console.log(cityName);
 
-// The initial AJAX call that identifies the user's searched city, grabs the latitude, longitude, and cityID.    
+    // The initial AJAX call that identifies the user's searched city, grabs the latitude, longitude, and cityID.    
     $.ajax({
         url:'https://developers.zomato.com/api/v2.1/locations?query=' + cityName,
         method: "GET",
@@ -35,13 +35,49 @@ $('#generate').on('click', function(){
             const restAddress = randRest.restaurant.location.address;
             const restZip = restAddress.slice(-5);
             const restHours = randRest.restaurant.timings;
+            const restPrice = randRest.restaurant.price_range;
             const restAVGfor2 = randRest.restaurant.average_cost_for_two;
             const restMenuLink = randRest.restaurant.menu_url;
-            
-        })
-    
-    })
+            console.log(restName);
+            console.log(restAddress);
+            console.log(restZip);
+            console.log(restHours);
+            console.log(restPrice);
+            console.log(restAVGfor2);
+            console.log(restMenuLink);
 
+            
+        //-----------------------------------------
+        //In the situation where we want to search by $$$$$
+            // const userDollarSigns = 2;
+            // const stuff = res.restaurants;
+            // console.log(stuff);
+            // // console.log(randRest);
+
+            // for (let i = 0; i < stuff.length; i++) {
+            //     // const element = array[i];
+            //     console.log(stuff[i].restaurant.price_range);
+            //     // console.log(userDollarSigns.toString());
+                
+            // // I'm commenting this if statement below because it's an infinite loop right now, but it's the first one I've written ehre that hasn't almost exploded my computer. Woops.
+            //     if (stuff[i].restaurant.price_range.indexOf(2) !== -1){
+            //         doStuff();
+            //         // const filteredRandRest = stuff[i];
+            //         // console.log(filteredRandRest);
+            
+            //     }
+            //     else {
+            //         const filteredRandRest = stuff[i];
+            //         console.log(filteredRandRest);
+            //     }
+
+            // };
+
+
+        });
+    
+    });
+  
 });
 
 //How the $$$$$ system will add into the page's search.
@@ -51,39 +87,34 @@ $('#generate').on('click', function(){
 
 
 //Front-end css stuff
-document.addEventListener('DOMContentLoaded', function(){
-    let stars = document.querySelectorAll('.star');
-    stars.forEach(function(star){
-        star.addEventListener('click', setRating); 
-    });
+// document.addEventListener('DOMContentLoaded', function(){
+//     let stars = document.querySelectorAll('.star');
+//     stars.forEach(function(star){
+//         star.addEventListener('click', setRating); 
+//     });
     
-    let rating = parseInt(document.querySelector('.stars').getAttribute('data-rating'));
-    let target = stars[rating - 1];
-    target.dispatchEvent(new MouseEvent('click'));
-});
-function setRating(ev){
-    let span = ev.currentTarget;
-    let stars = document.querySelectorAll('.star');
-    let match = false;
-    let num = 0;
-    stars.forEach(function(star, index){
-        if(match){
-            star.classList.pop('rated');
-        }else{
-            star.classList.add('rated');
-        }
-        //are we currently looking at the span that was clicked
-        if(star === span){
-            match = true;
-            num = index + 1;
-        }
-    });
-    document.querySelector('.stars').setAttribute('data-rating', num);
-}
-
-
-// id="star1"
-// id="star2"
-// id="star3"
-// id="star4"
-// id="star5"
+//     let rating = parseInt(document.querySelector('.stars').getAttribute('data-rating'));
+//     console.log(rating);
+//     let target = stars[rating - 1];
+//     console.log(target);
+//     target.dispatchEvent(new MouseEvent('click'));
+// });
+// function setRating(ev){
+//     let span = ev.currentTarget;
+//     let stars = document.querySelectorAll('.star');
+//     let match = false;
+//     let num = 0;
+//     stars.forEach(function(star, index){
+//         if(match){
+//             star.classList.pop('rated');
+//         }else{
+//             star.classList.add('rated');
+//         }
+//         //are we currently looking at the span that was clicked
+//         if(star === span){
+//             match = true;
+//             num = index + 1;
+//         }
+//     });
+//     document.querySelector('.stars').setAttribute('data-rating', num);
+// }
