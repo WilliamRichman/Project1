e5b34ae0b8172432568d14f037ee8ebb395678d6
Zac Eskridge <zace118@gmail.com>
2020-01-10 20:14:11 -0700

    added in functionality to get the distance between the user's location and the restaurants. However, now I need to push the RESTAURANT into a new array, not the distances.

--- script.js
+++ script.js
@@ -57,116 +57,171 @@ $('#generate').on('click', function(){
             console.log(filteredRestaurants);
 
             
-        // Declared variables to use for appending results to results div
-            const arrayLength = res.results_shown;
+        //This for loop loops through the filteredRestaurants array and creates variables for each filtered restaurant's latitude and longitude coordinates
+            const userDist = 7;
+            const distFilteredRestaurants = [];
 
-            //The "randRest" set to priceRange filter
-            const randRest = filteredRestaurants[Math.floor(Math.random()*(arrayLength))];
+            for (let i = 0; i < filteredRestaurants.length; i++) {
+                let randRestLat = filteredRestaurants[i].restaurant.location.latitude;
+                let randRestLon = filteredRestaurants[i].restaurant.location.longitude;
+                // console.log(randRestLat);
+                // console.log(randRestLon);
+            
+            // This AJAX call goes through Bing, takes the user's lat&lon and the random restaurant's lats&lons and gives us a result back, in miles. 
+                $.ajax({
+                    url: 'http://dev.virtualearth.net/REST/V1/Routes?waypoint.0=' + lat + ',' + lon + '&waypoint.1=' + randRestLat + ',' + randRestLon + '&distanceUnit=mi&key=Ags_JrYUkQjGiMOMLxaRXbGD8vihpzopc0mu62if5Q9ve5LBJR9YAN26jNb6ZfPT',
+                    method: 'GET'
+                }).then(function(rez){
+                    //  console.log(rez);
+                
+                    // filteredDist is giving me all the distances between the user selected city they search for OR their initial geolocation. 
+                    const filteredDist = rez.resourceSets[0].resources[0].travelDistance;
+                    console.log(filteredDist);
+
+                    // This compares the filteredDist's results and pushes anything less than or equal to the user selected distance into an empty array named distFiliteredRestaurants
+                    if (filteredDist <= userDist) {
+                        distFilteredRestaurants.push(filteredDist);
+                        // console.log(distFilteredRestaurants);
+                    }
+
+                    console.log(distFilteredRestaurants);
+
+                       //The "randRest" set to distance filter
+                        const arrayLength = distFilteredRestaurants.length;
+                        console.log(arrayLength);
+                        const randRest = distFilteredRestaurants[Math.floor(Math.random()*(arrayLength))];
+
+                        console.log(randRest);
+
+                })
+                
+            }
+            
+            console.log(distFilteredRestaurants);
+
+        // Declared variables to use for appending results to results div
+            // const arrayLength = res.results_shown;
+
             //The "randRest" set to not filters    
             // const randRest = res.restaurants[Math.floor(Math.random()*(arrayLength))];
-            console.log(randRest);
 
-            const restName = randRest.restaurant.name;
-            const restAddress = randRest.restaurant.location.address;
-            const restZip = restAddress.slice(-5);
-            const restHours = randRest.restaurant.timings;
-            const restPrice = randRest.restaurant.price_range;
-            const restAVGfor2 = randRest.restaurant.average_cost_for_two;
-            const restMenuLink = randRest.restaurant.menu_url;
+            //The "randRest" set to price range filter
+            // const randRest = filteredRestaurants[Math.floor(Math.random()*(arrayLength))];
 
-        //Appending the results to the page
-            $("#resultsDisplay").append('Restaurant Name: ' + restName + '<br>');
-            $("#resultsDisplay").append('Restaurant Address: ' + restAddress + '<br>');      
-            $("#resultsDisplay").append('Hours Open: ' + restHours + '<br>');
-            $("#resultsDisplay").append('Average Price for two: $' + restAVGfor2 + '<br>');
-            $("#menuLink").append('<a target="_blank" href="' + restMenuLink + '">Menu Link</a>');
+            
+
+
+
+
+
+
+//Uncomment out everything below to get site to work like normal before the distance filter stuff was added in 
+
+        //    //The "randRest" set to distance filter
+        //     const arrayLength = distFilteredRestaurants.length;
+        //     console.log(arrayLength);
+        //     const randRest = distFilteredRestaurants[Math.floor(Math.random()*(arrayLength))];
+
+
+
+        //     console.log(randRest);
+
+        //     const restName = randRest.restaurant.name;
+        //     const restAddress = randRest.restaurant.location.address;
+        //     const restZip = restAddress.slice(-5);
+        //     const restHours = randRest.restaurant.timings;
+        //     const restPrice = randRest.restaurant.price_range;
+        //     const restAVGfor2 = randRest.restaurant.average_cost_for_two;
+        //     const restMenuLink = randRest.restaurant.menu_url;
+
+        // //Appending the results to the page
+        //     $("#resultsDisplay").append('Restaurant Name: ' + restName + '<br>');
+        //     $("#resultsDisplay").append('Restaurant Address: ' + restAddress + '<br>');      
+        //     $("#resultsDisplay").append('Hours Open: ' + restHours + '<br>');
+        //     $("#resultsDisplay").append('Average Price for two: $' + restAVGfor2 + '<br>');
+        //     $("#menuLink").append('<a target="_blank" href="' + restMenuLink + '">Menu Link</a>');
         
 
             
-            console.log(restName);
-            console.log(restAddress);
-            console.log(restZip);
-            console.log(restHours);
-            console.log(restPrice);
-            console.log(restAVGfor2);
-            console.log(restMenuLink);
+        //     console.log(restName);
+        //     console.log(restAddress);
+        //     console.log(restZip);
+        //     console.log(restHours);
+        //     console.log(restPrice);
+        //     console.log(restAVGfor2);
+        //     console.log(restMenuLink);
            
 
-        //AJAX call to OpenWeather API using restZip
-            $.ajax({
-                url: 'https://api.openweathermap.org/data/2.5/weather?zip=' + restZip + ',us&units=imperial&appid=15976d84b292c4206f0104225b002459',
-                method: "GET"
-            }).then(function(response){
-                console.log(response);
-                const currentTemp = response.main.temp;
-                currentTemp.toFixed(0);
-                console.log(currentTemp.toFixed(0));
+        // // AJAX call to OpenWeather API using restZip
+        //     $.ajax({
+        //         url: 'https://api.openweathermap.org/data/2.5/weather?zip=' + restZip + ',us&units=imperial&appid=15976d84b292c4206f0104225b002459',
+        //         method: "GET"
+        //     }).then(function(response){
+        //         console.log(response);
+        //         const currentTemp = response.main.temp;
+        //         currentTemp.toFixed(0);
+        //         console.log(currentTemp.toFixed(0));
          
-                const currentWeather = response.weather[0].main
-                $("#resultsWeather").append('The current temperature is ' + currentTemp.toFixed(0) + ' and expect a ' + currentWeather + ' day');
-                console.log(currentWeather);
+        //         const currentWeather = response.weather[0].main
+        //         $("#resultsWeather").append('The current temperature is ' + currentTemp.toFixed(0) + ' and expect a ' + currentWeather + ' day');
+        //         console.log(currentWeather);
 
-                if (currentTemp < 30){
-                    console.log("Holy Guacamole! It's VERY cold! Bundle up!");
-                    $("#tempMessage").append("Holy Guacamole! It's VERY cold! Bundle up!");
-                }
-                if (currentTemp > 30.01 && currentTemp < 55){
-                    console.log("Brr! It's a bit chilly! Dress warmly.");
-                    $("#tempMessage").append("Brr! It's a bit chilly! Dress warmly.");
-                }
-                if (currentTemp > 55.01 && currentTemp < 80){
-                    console.log("Not too hot or too cold!");
-                    $("#tempMessage").append("Not too hot or too cold!");
-                }
-                if (currentTemp >80.01 && currentTemp < 100){
-                    console.log("Oof, it's getting warm. Wear something light!");
-                    $("#tempMessage").append("Oof, it's getting warm. Wear something light!");
-                }
+        //         if (currentTemp < 30){
+        //             console.log("Holy Guacamole! It's VERY cold! Bundle up!");
+        //             $("#tempMessage").append("Holy Guacamole! It's VERY cold! Bundle up!");
+        //         }
+        //         if (currentTemp > 30.01 && currentTemp < 55){
+        //             console.log("Brr! It's a bit chilly! Dress warmly.");
+        //             $("#tempMessage").append("Brr! It's a bit chilly! Dress warmly.");
+        //         }
+        //         if (currentTemp > 55.01 && currentTemp < 80){
+        //             console.log("Not too hot or too cold!");
+        //             $("#tempMessage").append("Not too hot or too cold!");
+        //         }
+        //         if (currentTemp >80.01 && currentTemp < 100){
+        //             console.log("Oof, it's getting warm. Wear something light!");
+        //             $("#tempMessage").append("Oof, it's getting warm. Wear something light!");
+        //         }
 
-                if (currentTemp >100.01 && currentTemp < 130){
-                    console.log("HOT, HOT, HOT!");
-                    $("#tempMessage").append("HOT, HOT, HOT!")
-                }
+        //         if (currentTemp >100.01 && currentTemp < 130){
+        //             console.log("HOT, HOT, HOT!");
+        //             $("#tempMessage").append("HOT, HOT, HOT!")
+        //         }
 
-                // seperated weather from temp for two messages that'll be more accurate based on conditions
+        //         // seperated weather from temp for two messages that'll be more accurate based on conditions
 
-                if (currentWeather === "Rain"){
-                    console.log("And don't forget your umbrella");
-                    $("#weatherMessage").append("And don't forget an umbrella");
-                    $("#resultsWeather").append('  <img id="rain" src="assets/img/rain2.png" />')
-                    $('#rain').width(50); // Units are assumed to be pixels
-                    $('#rain').height(50);
-                } 
+        //         if (currentWeather === "Rain"){
+        //             console.log("And don't forget your umbrella");
+        //             $("#weatherMessage").append("And don't forget an umbrella");
+        //             $("#resultsWeather").append('  <img id="rain" src="assets/img/rain2.png" />')
+        //             $('#rain').width(50); // Units are assumed to be pixels
+        //             $('#rain').height(50);
+        //         } 
 
-                if (currentWeather === "Clouds"){
-                    console.log("If it looks like it could rain then maybe grab an umbrella");
-                    $("#weatherMessage").append("If it looks like it could rain then maybe grab an umbrella");
-                    $("#resultsWeather").append('  <img id="clouds" src="assets/img/clouds.png" />')
-                    $('#clouds').width(50); // Units are assumed to be pixels
-                    $('#clouds').height(50);
-                }
+        //         if (currentWeather === "Clouds"){
+        //             console.log("If it looks like it could rain then maybe grab an umbrella");
+        //             $("#weatherMessage").append("If it looks like it could rain then maybe grab an umbrella");
+        //             $("#resultsWeather").append('  <img id="clouds" src="assets/img/clouds.png" />')
+        //             $('#clouds').width(50); // Units are assumed to be pixels
+        //             $('#clouds').height(50);
+        //         }
 
-                if (currentWeather === "Clear"){
-                    console.log("Looks like it'll be a beautiful day");
-                    $("#weatherMessage").append("Looks like it'll be a beautiful day");
-                    $("#resultsWeather").append('<img id="sunny" src="assets/img/sunny.png" />')
-                    $('#sunny').width(50); // Units are assumed to be pixels
-                    $('#sunny').height(50);
-                }
-<<<<<<< HEAD
-                if (currentTemp > 75.01 && currentTemp < 100){
-                    console.log("Oof, gettin warm. Wear somethin light!");
-=======
+        //         if (currentWeather === "Clear"){
+        //             console.log("Looks like it'll be a beautiful day");
+        //             $("#weatherMessage").append("Looks like it'll be a beautiful day");
+        //             $("#resultsWeather").append('<img id="sunny" src="assets/img/sunny.png" />')
+        //             $('#sunny').width(50); // Units are assumed to be pixels
+        //             $('#sunny').height(50);
+        //         }
 
-                if (currentWeather === "Snow"){
-                    console.log("Be careful as you travel!");
-                    $("#weatherMessage").append("Be careful as you travel!");
-                    $("#resultsWeather").append('<img id="snow" src="assets/img/snow.png" />')
-                    $('#snow').width(50); // Units are assumed to be pixels
-                    $('#snow').height(50);
->>>>>>> 2f880a59d63e27522b083c8d7d580845e0bb9f1b
-                }
-            });
+        //         if (currentWeather === "Snow"){
+        //             console.log("Be careful as you travel!");
+        //             $("#weatherMessage").append("Be careful as you travel!");
+        //             $("#resultsWeather").append('<img id="snow" src="assets/img/snow.png" />')
+        //             $('#snow').width(50); // Units are assumed to be pixels
+        //             $('#snow').height(50);
+        //         }
+        //     });
 
         });
     
